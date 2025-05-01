8fc22f5393a0e49a991a9bceb47c935dd4a2c52b
shaima-h <shaima.hussaini@gmail.com>
2025-05-01 16:25:42 -0400

    list formatting, MD

--- backend/intellinotes.py
+++ backend/intellinotes.py
@@ -70,8 +70,21 @@ def get_transcript(video_id: str):
 def summarize_text(text: str):
     """uses openai api to generate summary and key takeaways"""
     # Markdown
-    prompt = f"Generate notes for the video by providing:\n1. A concise summary of the main ideas.\n2. Key takeaways.\n3. Action steps (if applicable).\nDo not include any bulleted items or lists, instead, put each item on a different line WITHOUT a bullet point. Do not include any introductory or closing sentences.\nHere is the video: \n{text}"
+    prompt = f"""Generate notes for the video by providing:
+                1. A concise summary of the main ideas.
+                2. Key takeaways in a list.
+                3. Action steps in a list (if applicable).
 
+                Instructions:
+                Do not include any introductory or closing sentences.
+
+                For the lists:
+                - Do not use any bullets or dashes. Only use numbering (1, 2, 3, etc.) to order items if necessary.
+                - Put each item on a separate line.
+
+                Here is the video:
+                {text}
+                """
     # BBCode
     # prompt = f"Generate notes for the video by providing:\n1. A concise summary of the main ideas.\n2. Key takeaways in bullet points.\n3. Action steps (if applicable).\nGenerate your response in BBCode.\nOnly output the BBCode-formatted content. Do not include any introductory or closing sentences.\nHere is the video:\n{text}"
     
